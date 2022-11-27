f98d0f8496f4ce4c2decad449e5e1e3cafd57743
Paul Tiry <tirypaul@gmail.com>
2022-11-27 16:20:25 +0100

    routing setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,4 +6,8 @@ const Home = () => {
 
 export default Home
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  height: 1000px;
+  width: 1000px;
+  border: 1px solid black;
+`
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import GlobalStyles from './assets/styles/GlobalStyles'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <BrowserRouter>
+    <GlobalStyles />
+    <App />
+  </BrowserRouter>
+)
