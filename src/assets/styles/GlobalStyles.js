7ad1f18eb6ab6c78b8c1948fb42472572a53bfbc
Paul Tiry <tirypaul@gmail.com>
2022-11-27 18:55:04 +0100

    added Century Gothic font

--- src/assets/styles/GlobalStyles.js
+++ src/assets/styles/GlobalStyles.js
@@ -1,10 +1,31 @@
 import { createGlobalStyle } from 'styled-components'
+import CenturyGothic from '../fonts/century-gothic/CenturyGothic.ttf'
+import CenturyGothicBold from '../fonts/century-gothic/gothicb.ttf'
+import CenturyGothicItalic from '../fonts/century-gothic/gothici.ttf'
 
 export default createGlobalStyle`
+    @font-face {
+        font-family: 'CenturyGothic';
+        src: local('CenturyGothic'), url(${CenturyGothic});
+        font-weight: normal;
+    }
+    @font-face {
+        font-family: 'CenturyGothic';
+        src: local('CenturyGothicBold'), url(${CenturyGothicBold});
+        font-weight: 700;
+    }
+    @font-face {
+        font-family: 'CenturyGothic';
+        src: local('CenturyGothicItalic'), url(${CenturyGothicItalic});
+        font-style: italic;
+    }
     * {
         box-sizing: border-box;
         margin: 0;
         padding: 0;
+    }
+    body {
+        font-family: 'CenturyGothic', 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     }
     #root {
         width: 100vw;
